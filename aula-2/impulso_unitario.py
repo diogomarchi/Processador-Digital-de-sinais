c704d918b4a0d83c90ddd41f706cd46f150ccca4
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-09-03 22:24:02 -0300

    eco implementado, mas esta saindo ruido

--- aula-2/impulso_unitario.py
+++ aula-2/impulso_unitario.py
@@ -7,7 +7,7 @@ nLen = len(n)
 meuVetor = np.zeros((nLen, 1), dtype=np.int16)
 meuVetor[n == 0] = 1
 
-with open(r'impulsoUnitario', 'wb') as f:
+with open(r'impulsoUnitario.pcm', 'wb') as f:
     for d in meuVetor:
         f.write(d)
 
--- aula-3/eco.py
+++ aula-3/eco.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 #implementação do eco
 
 Fs = 8000
+Ts = 1 / Fs;
 t1 = 1.0*10**-3
 t2 = 1.5*10**-3
+L = 0.1 * Fs
 n1 = t1*Fs
 n2 = t2*Fs
 
@@ -13,12 +15,10 @@ n2 = t2*Fs
 a0 = 0.5
 a1 = 0.3
 a2 = 0.2
-tama_delay = int(n2)
-vetor_delay = np.zeros((tama_delay, 1))
 
 #Definindo a entrada
 
-with open('C:/Users/Diogo/Documents/FACULDADE-MATERIAS/Processador-Digital-de-sinais/aula-2/impulso_unitario.pcm', 'rb') as f:
+with open('C:/Users/Diogo/Documents/FACULDADE-MATERIAS/Processador-Digital-de-sinais/aula-1/alo.pcm', 'rb') as f:
     buf = f.read()
     data_i = np.frombuffer(buf, dtype='int16')
     data_len = len(data_i)
@@ -27,36 +27,36 @@ with open('C:/Users/Diogo/Documents/FACULDADE-MATERIAS/Processador-Digital-de-si
 
     for i in range(data_len):
         entrada[i] = data_i[i]
+    #f.close(buf)
 
 
-#definindo o impulso unitário
 tama_loop = len(entrada);
 vet_saida = np.zeros((tama_loop, 1))
+vetor_amostra = np.zeros((int(L), 1))
 
 for j in range(tama_loop):
-    input = entrada[j, 0]
-    vetor_delay[0, 0] = input
-    y = a0 * vetor_delay[0, 0] + a1 * vetor_delay[int(n1-1), 0] + a2 * vetor_delay[int(n2-1), 0]
-    for k in range(tama_delay-1):
-        vetor_delay[tama_delay-k-1, 0] = vetor_delay[tama_delay-k-2, 0]
-    vet_saida[j, 0] = y
+    vetor_amostra[0] = entrada[j];
+    y = a0 * vetor_amostra[0] + a1 * vetor_amostra[int(n1-1)] + a2 * vetor_amostra[int(n2-1)]
+    vet_saida[j] = y
+
+    for k in range(int(L)):
+        vetor_amostra[int(L)-k-1] = vetor_amostra[int(L)-k-2]
 
 plt.stem(vet_saida)
 plt.title('delay teste')
 plt.show()
 
-'''
-if __name__ == "__main__":
-    plt.close('all')
-    print("Selecione uma das seguintes opções: ")
-    option = input("'D': Degrau Unitario \n'I': Impulso Unitario \n'S': Sequencia Sinusoidal \n'E': Sequencia Exponencial\n\n")
-    if option == 'D' : degrau_unitario()
-    elif option == 'I': impulso_unitario()
-    elif option == 'S': sinusoidal()
-    elif option == 'E': exponencial()
-    else:
-        exit()
-'''
+with open('saidaEco.pcm', 'wb') as f:
+    for d in vet_saida:
+        f.write(d)
+
+
+
+
+
+
+
+
 
 
 
