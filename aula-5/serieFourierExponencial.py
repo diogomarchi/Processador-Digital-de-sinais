25de4adce0503f4e7f07b16c314c6150b53caa58
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-09-15 14:52:47 -0300

    serie de fourier exponencial implementado

---
+++ aula-5/serieFourierExponencial.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+w = np.arange(-1*np.pi, np.pi/100, 1*np.pi)
+# formula walter
+#a = 2*np.cos(w) + 1
+#a = 0.5
+#num = 1
+#den = 1 - a*np.exp(-1j*w)
+a = np.exp(1j*w) + 1 + np.exp(-1j*w)
+#x = num / den
+#mod_x = abs(x)
+mod_x = abs(a)
+#fase_x = np.angle(x)
+fase_x = np.angle(a)
+
+plt.subplot(2, 1, 1)
+plt.plot(w, mod_x)
+plt.title("Modulo de X")
+plt.xlabel("W")
+plt.grid
+
+plt.subplot(3, 1, 3)
+plt.plot(w, fase_x)
+plt.title("Fase de X")
+plt.xlabel("W")
+plt.grid
+
+plt.show()
