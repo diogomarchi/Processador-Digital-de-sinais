870b27647faac585974578c57c94d65e3e2c0185
diogomarchi <diogo.marchi@hotmail.com>
2020-11-17 12:15:17 -0300

    tentativa do cancelamento de eco

--- aula-12/cancelaEco.py
+++ aula-12/cancelaEco.py
@@ -8,43 +8,38 @@ with open("near_apcm.pcm", 'rb') as f:
     buf = f.read()
     data_i2 = np.frombuffer(buf, dtype='int16')
     data_lenNear = len(data_i2)
-    dn = data_i2[0:n]
+    near = data_i2[:n]
 
 #carregando ruido branco
 with open("far_apcm.pcm", 'rb') as f:
     buf = f.read()
     data_i = np.frombuffer(buf, dtype='int16')
     data_lenFar = len(data_i)
-    xn = data_i[0:n]
+    far = data_i[:n]
 
 
 #mi
-numero = 0.00000005
-
-#tamanho do filtro adaptativo
-tam_filtro = len(dn)
-
-#numero de interação pelo tamanho do ruido branco
-n_inter = 200000
+numero = 0.0000001
 
 # Vetores utilizados
-wn = np.zeros(tam_filtro, dtype=np.float64)			# Coeficientes de W(z)
+wn = np.zeros(n, dtype=np.float64)			# Coeficientes de W(z)
 
 erro = []
 
-for i in range(n_inter):
+for i in range(data_lenFar//n):
 
     # filtragem
-    yn = wn.T * xn
+    yn = wn.T * far
 
     # calculo do erro
-    ee = dn - yn
+    ee = near - yn
 
-    erro.append(sum(abs(ee)))
-    print(erro[i])
+    # atualizando coeficientes
+    wn = wn + 2 * numero * ee * far
 
-    #atualizando coeficientes
-    wn = wn + 2 * numero * ee * xn
+    erro.append(ee)
+
+
 
 # Plotando os sinais de erro e de controle
 plt.figure(1)
@@ -64,12 +59,6 @@ plt.show()
 # salva resultado do filtro
 file_name = "resultado_coefs_eco.pcm"
 with open(file_name, 'wb') as f:
-    yn = np.convolve(wn, data_i, mode="same")
-    for d in yn:
-        f.write(d.astype(np.float16))
-
-# salva coeficientes
-coefs_name = "coef_adptativo_eco.dat"
-with open(coefs_name, 'w') as f:
-    for d in wn:
-        f.write(str(d.astype(np.float16))+",\n")
+    erro = np.asarray(erro)
+    for d in erro:
+        f.write(d.astype(np.int16))
